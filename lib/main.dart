a51e1ed8bee9d7261310bd72b6b5631a5dcbfc4b
Guruprasad Naik <naikgrprsd@gmail.com>
2024-07-06 23:52:23 +0530

    Created app bar and drawer using scafold

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -9,12 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Material(
-      child: Center(
-        child: Container(
-          child: Text("Welcome to my first flutter app"),
-        ),
-    ))
-    );
+    return MaterialApp(home: Home());
   }
 }
